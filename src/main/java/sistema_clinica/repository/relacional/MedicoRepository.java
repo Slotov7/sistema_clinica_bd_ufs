44c3a7351375afdbfda2e63abd06d7312fac7609
Guilherme Araújo <132411540+Slotov7@users.noreply.github.com>
2025-08-25 22:32:31 -0300

    feat(repository): add @Repository annotation to repository interfaces

--- src/main/java/sistema_clinica/repository/relacional/MedicoRepository.java
+++ src/main/java/sistema_clinica/repository/relacional/MedicoRepository.java
@@ -1,11 +1,12 @@
 package sistema_clinica.repository.relacional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import sistema_clinica.model.relacional.Medico;
 import sistema_clinica.model.relacional.Usuario;
 
 import java.util.Optional;
 
-
+@Repository
 public interface MedicoRepository extends JpaRepository<Medico,Integer> {
 }
